012d8ac025e83c324c333a2cd5fdc3a1cd3e1949
yoshilinMC <yoshilin77@gmail.com>
2023-12-17 12:03:08 +0800

    W6

--- W5/HW.py
+++ W5/HW.py
@@ -2,10 +2,6 @@ class car:
     def __init__(self,name,age):
         self.name = name
         self.age = age
-        self.next = name
-        self.next = age
-        self.next = name
-        self.next = age
         self.next = None
 head = car("Amy",25)
 head.next = car("Eddy",43)
@@ -14,7 +10,7 @@ head.next.next = car("Esme",32)
 def traverse(head):
     ptr = head
     while ptr != None:
-        print("The employee name is is {} and the age is {}".format(ptr.name,ptr.age))
+        print("The employee name is {} and the age is {}".format(ptr.name,ptr.age))
         ptr = ptr.next
     print("Finish traverse")
 
